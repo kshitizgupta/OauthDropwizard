96f6059d10fa78ed3133e8cacb98599db16c2998
Kshitiz Gupta <kshitiz.gupta@izettle.com>
2020-06-08 00:38:13 +0530

    Adding cassandra to docker and adding authenticaion dao class and service class which basically dont do much as of now

--- server/src/main/java/com/kshitiz/oauthCore/auth/User.java
+++ server/src/main/java/com/kshitiz/oauthCore/auth/User.java
@@ -6,15 +6,18 @@ import java.util.Set;
 public class User implements Principal {
     private final String name;
     private final Set<String> roles;
+    private final String passwordHash;
 
-    public User(final String name) {
+    public User(final String name, final String passwordHash) {
         this.name = name;
+        this.passwordHash = passwordHash;
         this.roles = null;
     }
 
     public User(String name, Set<String> roles) {
         this.name = name;
         this.roles = roles;
+        passwordHash = null;
     }
     @Override
     public String getName() {
---
+++ server/src/main/java/com/kshitiz/oauthCore/dao/AuthenticationDao.java
@@ -0,0 +1,12 @@
+package com.kshitiz.oauthCore.dao;
+
+import com.kshitiz.oauthCore.auth.User;
+
+public class AuthenticationDao {
+    public void createUser(User user) {
+    }
+
+    public String getPasswordHash(String email) {
+        return "";
+    }
+}
